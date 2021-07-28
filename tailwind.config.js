0a4406242010840b3ff3cbf07e444020f3ee4a8e
Cernan Bernardo <cernanb@gmail.com>
2021-07-27 22:59:45 -0700

    Add custom orange shade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          50: "#fff9f4",
+          100: "#fef2e9",
+          200: "#fee0c9",
+          300: "#fdcda8",
+          400: "#fba766",
+          500: "#f98125",
+          600: "#e07421",
+          700: "#bb611c",
+          800: "#954d16",
+          900: "#7a3f12",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
